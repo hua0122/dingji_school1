82bde8b098edfb61f54c4f652dfdec34bb928926
VIPsth <VIPsongtaohong@yeah.net>
2018-11-29 14:19:42 +0800

    add

--- static/js/custom1.js
+++ static/js/custom1.js
@@ -7,7 +7,7 @@ if (/Android/gi.test(navigator.userAgent)) {
 		}
 	});
 }
-document.write('<script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>');
+document.write('<script src="http://res.wx.qq.com/open/js/jweixin-1.4.0.js"></script>');
 
 $(function() {
 
