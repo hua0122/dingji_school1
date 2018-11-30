3c26e80ceb423362195df0f3d838718f96652fc8
VIPsth <VIPsongtaohong@yeah.net>
2018-11-30 14:38:52 +0800

    add

--- static/js/custom1.js
+++ static/js/custom1.js
@@ -41,14 +41,14 @@ $(function() {
 $(function() {
 	pushHistory();
 	window.addEventListener("popstate", function(e) {
-		if (typeof document.referrer === '') {
-			// 没有来源页面信息的时候，改成首页URL地址
-			location.href = "/";
-
-		}else{
+// 		if (typeof document.referrer === '') {
+// 			// 没有来源页面信息的时候，改成首页URL地址
+// 			location.href = "/";
+// 
+// 		}else{
 			
 		location.href = document.referrer;
-		}
+		// }
 
 	}, false);
 
