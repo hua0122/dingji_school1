a8794f708217f3d3b0b55402b0f78cef19d5bea2
VIPsth <VIPsongtaohong@yeah.net>
2018-11-30 10:10:53 +0800

    add

--- static/js/custom1.js
+++ static/js/custom1.js
@@ -41,7 +41,7 @@ $(function() {
 $(function() {
 	pushHistory();
 	window.addEventListener("popstate", function(e) {
-		alert("我监听到了浏览器的返回按钮事件啦"); //根据自己的需求实现自己的功能  
+		alert(document.referrer); //根据自己的需求实现自己的功能  
 		location.href = document.referrer;
 	}, false);
 
