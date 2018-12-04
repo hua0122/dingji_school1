4f72b40886bd27fdb9d059aec57cf4b284560090
VIPsth <VIPsongtaohong@yeah.net>
2018-12-04 18:24:39 +0800

    add

--- static/js/custom1.js
+++ static/js/custom1.js
@@ -28,6 +28,7 @@ function inputblur(){
 	$('input').on('focus',function(e){
 		this.scrollIntoView();
 		document.getElementsByTagName('body')[0].scrollTop = document.getElementsByTagName('body')[0].scrollHeight;
+		document.getElementsByTagName('html')[0].scrollTop = document.getElementsByTagName('html')[0].scrollHeight;
 	});
 	$('input').on('blur',function( ){
 		$('body').scrollTop(0);
