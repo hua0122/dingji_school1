2df9d032efa146776af64f32f856c7c26e5237a1
VIPsth <VIPsongtaohong@yeah.net>
2018-12-05 17:45:52 +0800

    add

--- static/js/custom1.js
+++ static/js/custom1.js
@@ -78,18 +78,7 @@ function getydxc() {
 							}))
 						}
 					});
-					wx.onMenuShareQZone({
-						title: title, // 分享标题
-						desc: desc, // 分享描述
-						link: link, // 分享链接
-						imgUrl: imgUrl, // 分享图标
-						success: function() {
-							// 用户确认分享后执行的回调函数
-						},
-						cancel: function() {
-							// 用户取消分享后执行的回调函数
-						}
-					});
+					
 				});
 			} else {
 				//alert('系统繁忙！稍后再试！');
