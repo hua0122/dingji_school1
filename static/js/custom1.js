64b4eaeeaedd95b5980725e4b625eeaa9cf85aa7
VIPsth <VIPsongtaohong@yeah.net>
2018-12-12 18:34:54 +0800

    add

--- static/js/custom1.js
+++ static/js/custom1.js
@@ -85,16 +85,12 @@ function getydxc() {
 						link: link, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
 						imgUrl: imgUrl, // 分享图标
 						trigger: function(res) {
-							document.title = "用户点击分享到朋友圈";
 						},
 						success: function(res) {
-							document.title = "已分享";
 						},
 						cancel: function(res) {
-							document.title = "已取消";
 						},
 						fail: function(res) {
-								document.title = "xx";
 						}
 					});
 					wx.onMenuShareAppMessage({
@@ -103,17 +99,14 @@ function getydxc() {
 						link: link, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
 						imgUrl: imgUrl, // 分享图标
 						type: type, // 分享类型,music、video或link，不填默认为link
+						dataUrl: dataUrl, // 如果type是music或video，则要提供数据链接，默认为空
 						trigger: function(res) {
-							document.title = "用户点击分享到朋友圈";
 						},
 						success: function(res) {
-							document.title = "已分享";
 						},
 						cancel: function(res) {
-							document.title = "已取消";
 						},
 						fail: function(res) {
-								document.title = "xx";
 						}
 					});
 					wx.onMenuShareQQ({
@@ -146,16 +139,12 @@ function getydxc() {
 						link: link, // 分享链接
 						imgUrl: imgUrl, // 分享图标
 						trigger: function(res) {
-							document.title = "用户点击分享到朋友圈";
 						},
 						success: function(res) {
-							document.title = "已分享";
 						},
 						cancel: function(res) {
-							document.title = "已取消";
 						},
 						fail: function(res) {
-								document.title = "xx";
 						}
 					});
 				});
