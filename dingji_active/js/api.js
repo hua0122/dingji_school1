e297c612ab2411b46cde173c4be440023928c892
VIPsth <VIPsongtaohong@yeah.net>
2018-12-20 19:03:07 +0800

    add

--- dingji_active/js/api.js
+++ dingji_active/js/api.js
@@ -221,8 +221,8 @@ function prestore_h5() {
 	}
 	let data = ajaxPost(activity_prestore_h5, ajaxdata);
 	if (data.status == "200") {
-		console.log(data.data.package)
-		// location.href=data.data.mweb_url;
+		console.log("https://wx.tenpay.com/cgi-bin/mmpayweb-bin/checkmweb?"+data.data.package)
+		// location.href="https://wx.tenpay.com/cgi-bin/mmpayweb-bin/checkmweb?"+data.data.package;
 
 	} else {
 		layer.open({
