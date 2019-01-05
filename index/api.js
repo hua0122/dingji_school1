9559bdf22bc0ea675524801b4cc9ee482dd87ade
VIPsth <VIPsongtaohong@yeah.net>
2019-01-05 18:35:55 +0800

    add

--- index/api.js
+++ index/api.js
@@ -56,12 +56,12 @@ function get_banner() {
 		cuteslider3.api.addEventListener(Cute.SliderEvent.CHANGE_END, function(event) {});
 		cuteslider3.api.addEventListener(Cute.SliderEvent.WATING, function(event) {});
 		cuteslider3.api.addEventListener(Cute.SliderEvent.CHANGE_NEXT_SLIDE, function(event) {});
-		cuteslider3.api.addEventListener(Cute.SliderEvent.WATING_FOR_NEXT, function(event) {setTimeout(function(){
+		cuteslider3.api.addEventListener(Cute.SliderEvent.WATING_FOR_NEXT, function(event) {
 			$("#cuteslider_3_wrapper").css({
 				"background": ""
 			})
-		},7000)});
-		
+		});
+
 		$(".br-thumb-up").remove();
 	}
 	// document.querySelector("#cuteslider_3").style.height="100%";
